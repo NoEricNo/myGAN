307ff1b4389d997f8fe451c6df3c0b9627f9c318
Eric <142195062+NoEricNo@users.noreply.github.com>
2024-05-31 03:40:03 -0400

    In case you want to keep consistency and always use csv. I think .dat files are more professional. Oh, well.

---
+++ dat_to_csv.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+input_file = 'datasets/ratings.dat'
+output_file = 'datasets/ratings.csv'
+column_names = ['userId', 'movieId', 'rating', 'timestamp']
+df = pd.read_csv(input_file, sep='::', engine='python', names=column_names)
+df.to_csv(output_file, index=False)
+print(f'File {output_file} has been created successfully.')
