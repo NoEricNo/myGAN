18bbad694fc6dcd82bb0be8685280a2e3f5d8c0d
Eric <142195062+NoEricNo@users.noreply.github.com>
2024-06-01 23:11:13 -0400

    removing useless files

--- dat_to_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-input_file = 'datasets/ratings_1m.dat'
-output_file = 'datasets/ratings_1m.csv'
-column_names = ['userId', 'movieId', 'rating', 'timestamp']
-df = pd.read_csv(input_file, sep='::', engine='python', names=column_names)
-df.to_csv(output_file, index=False)
-print(f'File {output_file} has been created successfully.')
